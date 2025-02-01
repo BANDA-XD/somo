ca7edb86c92965e23da06cceba1ee71b9fbe1361
BANDA-XD <mohamed.banda.xd@gmail.com>
2025-02-01 13:33:40 +0100

    Update somo.py

--- somo.py
+++ somo.py
@@ -4,7 +4,7 @@ bit = platform.architecture()[0]
 if bit == '64bit':
     os.system('clear')
     os.system('git pull')
-    import BANDA64  
+    import BANDA32  
 elif bit == '32bit':
     os.system('clear')
     os.system('git pull')
