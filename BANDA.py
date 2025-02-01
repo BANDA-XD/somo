6eb230d09c9648aed52644a0d1587cabb3d3ab31
BANDA-XD <mohamed.banda.xd@gmail.com>
2025-02-01 13:48:19 +0100

    BANDA.py

--- BANDA.py
+++ BANDA.py
@@ -1,11 +1,4 @@
-import os, platform, time, sys
-os.system('xdg-open https://chat.whatsapp.com/IaOWZvh4B0jB5878FiLJYO')
-print('\033[1;97m[\033[1;91m+\033[1;91m] \033[1;97mChecking For Update...')
-os.system('git pull --quiet 2>/dev/null')
-ranaxd = platform.architecture()[0]
-if ranaxd == '64bit':
- print('\033[1;97m[\033[1;91m+\033[1;91m] \033[1;97mYour Device is 64bit')
- import somo
-elif ranaxd == '32bit':
- print('\033[1;97m[\033[1;91m+\033[1;91m] \033[1;97mYour Devive is 32bit')
- import BANDA
+import os
+os.system("git pull")
+os.system('xdg-open https://chat.whatsapp.com/ImXknPvyvU9IFBballhWgy')
+import BANDA
